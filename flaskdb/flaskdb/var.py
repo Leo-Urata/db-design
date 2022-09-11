550c5fc77fcfd2a46ed6af3d9e4d2931ecce88c8
KeitaWata <74957678+KeitaWata@users.noreply.github.com>
2022-09-11 20:54:10 +0900

    Update var.py

--- flaskdb/flaskdb/var.py
+++ flaskdb/flaskdb/var.py
@@ -5,7 +5,7 @@ Copyright (C) 2022 Yasuhiro Hayashi
 DBTYPE   = "postgresql"
 HOSTNAME = "127.0.0.1"
 PORT     = 5433
-DBNAME   = "flaskdb2"
+DBNAME   = "flaskdb"
 USERNAME = "postgres"
 PASSWORD = "kei0921wata"
 SHOW_SQL = True
